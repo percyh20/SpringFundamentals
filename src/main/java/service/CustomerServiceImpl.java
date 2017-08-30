3cf856c70f0ebb18aae69355f7666711d060b736
Percyh20 <percyherrera@hotmail.com>
2017-08-30 17:31:06 -0600

    last xml configurations examples

--- src/main/java/service/CustomerServiceImpl.java
+++ src/main/java/service/CustomerServiceImpl.java
@@ -7,15 +7,21 @@ import java.util.List;
 public class CustomerServiceImpl implements interfaces.CustomerService {
     private CustomerRepository customerRepository ;
 
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    public CustomerServiceImpl(){
+
     }
 
+    public CustomerServiceImpl (CustomerRepository customerRepo){
+        this.customerRepository = customerRepo;
+    }
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.findAll();
     }
 
+    public void setCustomerRepo(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
 }
